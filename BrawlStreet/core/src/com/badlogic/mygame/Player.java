c20197717115e490cd3cb2a5d765b167f401b333
Stzhino <98289156+Stzhino@users.noreply.github.com>
2022-05-31 22:51:12 -0400

    two new classes (wip)

---
+++ BrawlStreet/core/src/com/badlogic/mygame/Player.java
@@ -0,0 +1,6 @@
+package com.badlogic.mygame;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class Player extends Actor {
+}
--- BrawlStreet/core/src/com/badlogic/mygame/SettingsScreen.java
+++ BrawlStreet/core/src/com/badlogic/mygame/SettingsScreen.java
@@ -1,9 +1,11 @@
 package com.badlogic.mygame;
 
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Texture;
 
 public class SettingsScreen extends ScreenAdapter {
     private GameRunner game;
+    private Texture bg;
     public SettingsScreen(GameRunner game){
         this.game = game;
     }
